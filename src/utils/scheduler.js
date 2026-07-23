07080728f2e6b4d9576325edf0b596cc902f8742
Nirmal <nirmalbabu6152@gmail.com>
2026-07-23 10:09:44 +0530

    feat(scheduler): trigger immidiate job expiry check on scheduler initialization

--- src/utils/scheduler.js
+++ src/utils/scheduler.js
@@ -1,6 +1,13 @@
 const { startJobExpiryCron } = require("../jobs/jobexpiryCron");
+const { expirePastDeadlineJob } = require("../services/job.service");
 
-module.exports.initSchedulers = () => {
+module.exports.initSchedulers = async () => {
+    try {
+        const { result: missedCount } = await expirePastDeadlineJob();
+        console.log(`[STARTUP] Catch-up complete. Processed ${missedCount} jobs.`);
+    } catch (error) {
+        console.log("[SCHEDULER]: Error initializing schedulers", error)
+    }
+
     startJobExpiryCron();
-    console.log("[SCHEDULER]: Schedulers Initialized")
 }
